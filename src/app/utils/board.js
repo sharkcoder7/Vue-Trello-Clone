42095ff35c193970add961061d19f1eb9c1069dd
denebtech <techdeneb@gmail.com>
2019-02-12 07:35:24 -0500

    Move state management to Vuex

--- src/app/utils/board.js
+++ src/app/utils/board.js
@@ -1,3 +1,5 @@
+const uuidv1 = require('uuid/v1')
+
 /**
  * Helper function to update array on drop
  *
@@ -8,7 +10,7 @@
  * @param {*}       event.payload       Any payload associated with the move
  * @returns {*}
  */
-export const applyDrag = (arr, event) => {
+export function updateArray (arr, event) {
   const { removedIndex, addedIndex, payload } = event
   if (removedIndex === null && addedIndex === null) return arr
 
@@ -26,13 +28,45 @@ export const applyDrag = (arr, event) => {
   return result
 }
 
-export function getNextId (arr) {
-  const id = arr.reduce((id, item) => {
-    if (item.id > id) {
-      id = item.id
-    }
-    return id
-  }, 0)
-  return id + 1
+/**
+ * Helper function to update array on drop
+ *
+ * @param {array}   oldArray    The array to be altered
+ * @param {number}  removedIndex  The index the element moved from
+ * @param {number}  addedIndex    The index the element moved to
+ * @param {*}       payload       Any payload associated with the move
+ * @returns {*}
+ */
+export function updateArray2 (oldArray, removedIndex, addedIndex, payload) {
+  if (removedIndex === null && addedIndex === null) return oldArray
+
+  const newArray = [...oldArray]
+  let item = payload
+
+  if (removedIndex !== null) {
+    item = newArray.splice(removedIndex, 1)[0]
+  }
+
+  if (addedIndex !== null) {
+    newArray.splice(addedIndex, 0, item)
+  }
+
+  return newArray
 }
 
+export function getListById (lists, listId) {
+  return lists.find(list => list.id === listId)
+}
+
+export function card (title, description, date) {
+  const id = uuidv1()
+  if (date) {
+    date = Number(new Date(date))
+  }
+  return { id, title, description, date }
+}
+
+export function list (title, items = []) {
+  const id = uuidv1()
+  return { id, title, items }
+}
--- src/app/views/components/Board.vue
+++ src/app/views/components/Board.vue
@@ -45,36 +45,19 @@
             <input placeholder="Add a list" class="input" @keydown.enter="onAddList"/>
           </section>
         </Draggable>
+
       </Container>
 
     </div>
 
-    <ui-modal :active="modal" @close="modal = false" ref="modal">
-      <div class="query-form card">
-        <div class="card-content">
-          <h2 class="title">New task</h2>
-          <ui-input ref="title"
-                    label="Title"
-                    v-model="form.title"
-                    @enter="onAddFullItem"
-          />
-          <ui-input ref="description"
-                    label="Description"
-                    v-model="form.description"
-                    type="textarea"
-          />
-          <ui-input ref="date"
-                    label="Date"
-                    v-model="form.date"
-                    type="date"
-                    @enter="onAddFullItem"
-          />
-          <div class="field is-grouped">
-            <ui-button @click="onAddFullItem">Add</ui-button>
-            <ui-button type="text" @click="onCancelFullItem">Cancel</ui-button>
-          </div>
-        </div>
-      </div>
+    <ui-modal ref="modal"
+              :active="modal"
+              :cancellable="1"
+              @close="onModalClose"
+    >
+      <UiItemForm ref="form"
+                  @submit="onAddFullItem"
+                  @cancel="onModalClose"/>
     </ui-modal>
 
   </div>
@@ -83,135 +66,52 @@
 
 <script>
 import { Container, Draggable } from 'vue-smooth-dnd'
-import { applyDrag } from 'app/utils/board'
 
 import Card from './Card'
 import UiItemForm from '../ui/UiItemForm'
 import UiItemEntry from '../ui/UiItemEntry'
 
-let id = 0
-
-function card (title, description, date) {
-  id++
-  if (date) {
-    date = Number(new Date(date))
-  }
-  return {id, title, description, date}
-}
-
-function list (title, items = []) {
-  id++
-  return {id, title, items}
-}
-
-const dummy = [
-  list('One', [
-    card('1 1'),
-    card('1 2'),
-    card('1 3'),
-  ]),
-  list('Two', [
-    card('2 1'),
-    card('2 2'),
-    card('2 3'),
-  ]),
-  list('Three', [
-    card('3 1'),
-    card('3 2'),
-    card('3 3'),
-  ]),
-]
-
-function form () {
-  return {
-    listId: null,
-    title: '',
-    description: '',
-    date: null
-  }
-}
-
 export default {
   components: {
     Container,
     Draggable,
-    Card,
     UiItemEntry,
     UiItemForm,
+    Card,
   },
 
   data: function () {
     return {
       modal: false,
-      form: form(),
-      lists: dummy
+      activeListId: null,
     }
+  },
+
+  computed: {
+    lists () {
+      return this.$store.state.board.lists
+    }
+  },
+
+  mounted () {
+    this.$store.dispatch('load', true)
   },
 
   methods: {
     onListDrop: function (event) {
-      this.lists = applyDrag(this.lists, event)
+      this.$store.commit('moveList', event)
     },
 
     onCardDrop: function (listId, event) {
       if (event.removedIndex !== null || event.addedIndex !== null) {
-        // find containing list of item
-        const list = this.getListById(listId)
-
-        // copy list and move item
-        const newList = Object.assign({}, list)
-        newList.items = applyDrag(newList.items, event)
-
-        // replace old list with copy
-        const listIndex = this.lists.indexOf(list)
-        this.lists.splice(listIndex, 1, newList)
+        this.$store.commit('moveItem', { listId, ...event })
       }
     },
 
     onAddList (event) {
-      const text = event.target.value
+      const title = event.target.value
       event.target.value = ''
-      this.addList(text)
-    },
-
-    onAddItem ({id, text, more}) {
-      if (more) {
-        this.modal = true
-        this.form.title = text
-        this.form.listId = id
-        this.$nextTick(() => {
-          // focus first modal field
-          this.$refs.modal.$el.querySelector('input').focus()
-        })
-        return
-      }
-      this.addCard(id, text)
-    },
-
-    onAddFullItem () {
-      const data = this.form
-      this.addCard(data.listId, data.title, data.description, data.date)
-      this.onCancelFullItem()
-    },
-
-    onCancelFullItem () {
-      this.focusInput(this.form.listId)
-      this.form = form()
-      this.modal = false
-    },
-
-    getItemPayload: function (listId) {
-      return index => {
-        return this.getListById(listId).items[index]
-      }
-    },
-
-    getListById (listId) {
-      return this.lists.find(list => list.id === listId)
-    },
-
-    addList (text) {
-      this.lists.push(list(text))
+      this.$store.commit('addList', {title})
       this.$nextTick(() => {
         const lists = this.$refs.list
         lists[lists.length - 1]
@@ -220,8 +120,39 @@ export default {
       })
     },
 
-    addCard (listId, text, description, date) {
-      this.getListById(listId).items.push(card(text, description, date))
+    onAddItem ({id, text, more}) {
+      if (more) {
+        this.activeListId = id
+        this.modal = true
+        this.$nextTick(() => {
+          this.$refs.form.show({
+            title: text
+          })
+        })
+        return
+      }
+      this.addItem(id, text)
+    },
+
+    onAddFullItem (data) {
+      this.addItem(this.activeListId, data.title, data.description, data.date)
+      this.onModalClose()
+    },
+
+    onModalClose () {
+      this.focusInput(this.activeListId)
+      this.modal = false
+    },
+
+    addItem (listId, title, description, date) {
+      this.$store.commit('addItem', { listId, title, description, date })
+    },
+
+    getItemPayload: function (listId) {
+      return index => {
+        const list = this.$store.getters['getListById'](listId)
+        return list.items[index]
+      }
     },
 
     focusInput (listId) {
